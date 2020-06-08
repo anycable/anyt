ed106763c8e860c60f0b70dc80fb483646803662
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-06-08 18:10:52 +0300

    feat: add channel state test

---
+++ lib/anyt/tests/features/channel_state_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+feature "Channel state" do
+  channel do
+    include AnyCable::Rails::ChannelState
+
+    state_attr_accessor :user, :count
+
+    def subscribed
+      self.user = {name: params["name"]}
+      self.count = 1
+    end
+
+    def tick
+      self.count += 2
+      transmit(count: count, name: user[:name])
+    end
+  end
+
+  let(:identifier) { {channel: channel, name: "chipolino"}.to_json }
+
+  before do
+    subscribe_request = {command: "subscribe", identifier: identifier}
+
+    client.send(subscribe_request)
+
+    ack = {
+      "identifier" => identifier, "type" => "confirm_subscription"
+    }
+
+    assert_equal ack, client.receive
+  end
+
+  scenario %(
+    Channel state is kept between commands
+  ) do
+    perform_request = {
+      :command => "message",
+      :identifier => identifier,
+      "data" => {"action" => "tick"}.to_json
+    }
+
+    client.send(perform_request)
+
+    msg = {"identifier" => identifier, "message" => {"count" => 3, "name" => "chipolino"}}
+
+    assert_equal msg, client.receive
+  end
+end
