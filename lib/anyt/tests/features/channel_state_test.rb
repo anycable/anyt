ba8cc1075c196ab16033e575e0b0a5bdfaebc07e
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-09-08 13:45:58 +0300

    Add channel state #unsubscribed test

--- lib/anyt/tests/features/channel_state_test.rb
+++ lib/anyt/tests/features/channel_state_test.rb
@@ -7,15 +7,24 @@ feature "Channel state" do
     def subscribed
       self.user = {name: params["name"]}
       self.count = 1
+
+      stream_from "state_counts"
     end
 
     def tick
       self.count += 2
       transmit(count: count, name: user[:name])
     end
+
+    def unsubscribed
+      ActionCable.server.broadcast("state_counts", data: "user left: #{user[:name]}")
+    end
   end
 
   let(:identifier) { {channel: channel, name: "chipolino"}.to_json }
+
+  let(:client2) { build_client(ignore: %w[ping welcome]) }
+  let(:identifier2) { {channel: channel, name: "chipollone"}.to_json }
 
   before do
     subscribe_request = {command: "subscribe", identifier: identifier}
@@ -43,5 +52,28 @@ feature "Channel state" do
     msg = {"identifier" => identifier, "message" => {"count" => 3, "name" => "chipolino"}}
 
     assert_equal msg, client.receive
+  end
+
+  scenario %(
+    Channel state is available in #unsubscribe callbacks
+  ) do
+    subscribe_request = {command: "subscribe", identifier: identifier2}
+
+    client2.send(subscribe_request)
+
+    ack = {
+      "identifier" => identifier2, "type" => "confirm_subscription"
+    }
+
+    assert_equal ack, client2.receive
+
+    client.close
+
+    msg = {
+      "identifier" => identifier2,
+      "message" => {"data" => "user left: chipolino"}
+    }
+
+    assert_equal msg, client2.receive
   end
 end
